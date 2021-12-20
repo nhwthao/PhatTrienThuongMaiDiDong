6668d3c147264cc39aa10e51820e361e87003d4c
DiemChau2201 <chaugk14cpt@gmail.com>
2021-12-20 21:47:15 +0700

    Gan Navigation va fragment

--- Back up/WasabiApp/app/src/main/java/com/example/wasabiapp/ActivityHome.java
+++ Back up/WasabiApp/app/src/main/java/com/example/wasabiapp/ActivityHome.java
@@ -2,7 +2,10 @@ package com.example.wasabiapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
@@ -21,6 +24,11 @@ import android.widget.Toast;
 
 import com.example.adapter.BannerAdapter;
 import com.example.adapter.ViewTinTucAdapter;
+import com.example.fragment.FragmentCaledar;
+import com.example.fragment.FragmentFile;
+import com.example.fragment.FragmentHome;
+import com.example.fragment.FragmentNotice;
+import com.example.fragment.FragmentUser;
 import com.example.model.Banner;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
@@ -29,117 +37,79 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ActivityHome extends AppCompatActivity {
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    RecyclerView rcvBanner;
-    ArrayList<Banner> banners;
-    BannerAdapter adapter;
-    String[] items;
-    ArrayList<String> listItems;
-    ArrayAdapter<String> adapterSearch;
-    ListView lvSearch;
-    EditText edtSearch;
+    private ViewPager viewPager_home;
+    private BottomNavigationView bottomNavigationView;
+    int FragmentHome = 0;
+    int FragmentCalendar = 1;
+    int FragmentFile = 2;
+    int FragmentNotice = 3;
+    int FragmentUser = 4;
+    int CurrentFragment = FragmentHome;
+    BottomNavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        linkViews();
-        configRecyclerView();
-        initData();
-//        navigation();
-        tabLayout =findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_paper);
-        ViewTinTucAdapter viewTinTucAdapter = new ViewTinTucAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        viewPager.setAdapter(viewTinTucAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-//        addEvents();
-//        lvSearch = findViewById(R.id.lvSearch);
-        //lvSearch.setVisibility(View.INVISIBLE);
+        navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnItemSelectedListener(item -> {
+            swapScreen(item);
+            return false;
+        });
+        Fragment previousFragment = new FragmentHome();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.view_paper_nav, previousFragment);
+        fragmentTransaction.commit();
 
-        edtSearch = findViewById(R.id.edtSearch);
-//        initList();
-//        edtSearch.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int star, int count, int after) {
-//                ActivityHome.this.adapterSearch.getFilter().filter(s);
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//
-//            }
-//        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.view_paper_nav, fragment).addToBackStack("my_fragment");
+        fragmentTransaction.commit();
     }
 
-//    private void navigation() {
-//        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
-//        navigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
-//            @Override
-//            public void onNavigationItemReselected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.action_home:
-//                        Toast.makeText(ActivityHome.this, "Home", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case R.id.action_calendar:
-//                        Toast.makeText(ActivityHome.this, "Calendar", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case R.id.action_file:
-//                        Toast.makeText(ActivityHome.this, "File", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case R.id.action_notification:
-//                        Toast.makeText(ActivityHome.this, "notification", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case R.id.action_person:
-//                        Toast.makeText(ActivityHome.this, "person", Toast.LENGTH_SHORT).show();
-//                        break;
-//                }
-//                return;
-//            }
-//        });
-//    }
+    private void swapScreen(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                if (FragmentHome != CurrentFragment) {
+                    replaceFragment(new FragmentHome());
+                    CurrentFragment = FragmentHome;
+                    navigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+                }
+                break;
+            case R.id.action_calendar:
+                if (FragmentCalendar != CurrentFragment) {
+                    replaceFragment(new FragmentCaledar());
+                    CurrentFragment = FragmentCalendar;
+                    navigationView.getMenu().findItem(R.id.action_calendar).setChecked(true);
+                }
+                break;
 
-//    private void initList() { items = new String[]{
-//            "Đặt khám bác sĩ",
-//            "Đặt khám chuyên khoa",
-//            "Đặt khám bệnh viện",
-//            "Đặt khám phòng khám",
-//            "Tư vấn y tế online",
-//            "Diễn đàn y tế"
-//    };
-//        listItems = new ArrayList<>(Arrays.asList(items));
-//        adapterSearch = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
-//        lvSearch.setAdapter(adapterSearch);
-//    }
-//
-//    private void addEvents() {
-//    }
+            case R.id.action_file:
+                if (FragmentFile != CurrentFragment) {
+                    replaceFragment(new FragmentFile());
+                    CurrentFragment = FragmentFile;
+                    navigationView.getMenu().findItem(R.id.action_file).setChecked(true);
+                }
+                break;
 
-    private void configRecyclerView() {
-        //thiết lập hướng cho rcvBanner
-        LinearLayoutManager manager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
-        rcvBanner.setLayoutManager(manager);
-    }
-
-    private void initData() { //khởi tạo dữ liệu
-        banners= new ArrayList<>();
-        banners.add(new Banner(R.drawable.nf1));
-        banners.add(new Banner(R.drawable.nf2));
-        banners.add(new Banner(R.drawable.nf3));
-        banners.add(new Banner(R.drawable.nf4));
-        banners.add(new Banner(R.drawable.nf5));
-        adapter=new BannerAdapter(getApplicationContext(),banners);
-        rcvBanner.setAdapter(adapter);
-    }
-
-
-    private void linkViews() {
-        rcvBanner=findViewById(R.id.rcvBanner);
-
+            case R.id.action_notification:
+                if (FragmentNotice != CurrentFragment) {
+                    replaceFragment(new FragmentNotice());
+                    CurrentFragment = FragmentNotice;
+                    navigationView.getMenu().findItem(R.id.action_notification).setChecked(true);
+                }
+                break;
+            case R.id.action_person:
+                if (FragmentUser != CurrentFragment) {
+                    replaceFragment(new FragmentUser());
+                    CurrentFragment = FragmentUser;
+                    navigationView.getMenu().findItem(R.id.action_person).setChecked(true);
+                }
+                break;
+        }
     }
 }
