f108b2dc3800227b6a964d64f8ceafe49480b859
nhwthao <thaohtn19411@st.uel.edu.vn>
2021-12-21 13:29:15 +0700

    trans to fragment notification, user

--- Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentNotification.java
+++ Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentNotification.java
@@ -4,22 +4,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.model.BookAppointment;
+import com.example.model.MyItemClick;
 import com.example.wasabiapp.R;
 
 public class FragmentNotification extends Fragment {
-
-    public FragmentNotification(){
-
-    }
+    Button btnTatCa, btnLichKham, btnYTe;
+    private FragmentManager manager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+        btnTatCa = view.findViewById(R.id.btnTatCa);
+        btnLichKham = view.findViewById(R.id.btnLichKham);
+        btnYTe = view.findViewById(R.id.btnYTe);
+        addEvents();
+        return view;
     }
+
+    private void addEvents() {
+        btnTatCa.setOnClickListener(myClick);
+        btnLichKham.setOnClickListener(myClick);
+        btnYTe.setOnClickListener(myClick);
+    }
+    View.OnClickListener myClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+            Fragment fragment = null;
+            if(view.getId() == R.id.btnTatCa){
+                fragment = new FragmentTBTatCa();
+            }
+            if(view.getId() == R.id.btnLichKham){
+                fragment = new FragmentTBLichKham();
+            }
+            if(view.getId() == R.id.btnYTe){
+                fragment = new FragmentTBYTe();
+            }
+            transaction.replace(R.id.layoutContainerTB,fragment);
+            transaction.commit();
+        }
+    };
+
 }
--- Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentUser.java
+++ Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentUser.java
@@ -7,66 +7,56 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.model.BookAppointment;
+import com.example.model.MyItemClick;
+//import com.example.wasabiapp.ActivityLogout;
 import com.example.wasabiapp.R;
 
 import com.example.wasabiapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentUser#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentUser extends Fragment {
+    TextView txtLogout;
+    ImageView imvLogout;
+    private FragmentManager manager;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentUser() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentUser.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentUser newInstance(String param1, String param2) {
-        FragmentUser fragment = new FragmentUser();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        txtLogout = view.findViewById(R.id.txtLogout);
+        imvLogout = view.findViewById(R.id.imvLogout);
+        addEvents();
+        return view;
+    }
+
+    private void addEvents() {
+        txtLogout.setOnClickListener(myClick);
+        imvLogout.setOnClickListener(myClick);
+    }
+    View.OnClickListener myClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+//            FragmentActivity transaction = getActivity();
+//            ActivityLogout fragment = null;
+//            if(view.getId() == R.id.txtLogout){
+//                fragment = new ActivityLogout();
+//            }
+//            if(view.getId() == R.id.imvLogout) {
+//                fragment = new ActivityLogout();
+//            }
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
-    }
+    };
 }
+
