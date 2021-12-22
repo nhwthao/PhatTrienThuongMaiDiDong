f870615466d8be39fab145a27d70353839a744b2
nhwthao <thaohtn19411@st.uel.edu.vn>
2021-12-22 21:55:13 +0700

    Them FragmentLogout

---
+++ Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentLogout.java
@@ -0,0 +1,44 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.wasabiapp.R;
+
+
+public class FragmentLogout extends Fragment {
+    Button btnYes, btnNo;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        btnYes = view.findViewById(R.id.btnYes);
+        btnNo = view.findViewById(R.id.btnNo);
+        addEvents();
+        return view;
+    }
+
+    private void addEvents() {
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
--- Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentUser.java
+++ Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentUser.java
@@ -1,5 +1,6 @@
 package com.example.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,42 +23,35 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.model.BookAppointment;
 import com.example.model.MyItemClick;
 //import com.example.wasabiapp.ActivityLogout;
+import com.example.wasabiapp.ActivityLoginAndRegister;
 import com.example.wasabiapp.R;
 
 import com.example.wasabiapp.R;
 
 public class FragmentUser extends Fragment {
-    TextView txtLogout;
-    ImageView imvLogout;
-    private FragmentManager manager;
-
+    LinearLayout lnLogout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
-        txtLogout = view.findViewById(R.id.txtLogout);
-        imvLogout = view.findViewById(R.id.imvLogout);
+        lnLogout = view.findViewById(R.id.lnLogout);
         addEvents();
         return view;
     }
 
+
     private void addEvents() {
-        txtLogout.setOnClickListener(myClick);
-        imvLogout.setOnClickListener(myClick);
+        lnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentLogout fragmentLogout = new FragmentLogout();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frUser, fragmentLogout);
+                transaction.commit();
+            }
+        });
+
     }
-    View.OnClickListener myClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-//            FragmentActivity transaction = getActivity();
-//            ActivityLogout fragment = null;
-//            if(view.getId() == R.id.txtLogout){
-//                fragment = new ActivityLogout();
-//            }
-//            if(view.getId() == R.id.imvLogout) {
-//                fragment = new ActivityLogout();
-//            }
-        }
-    };
 }
 
