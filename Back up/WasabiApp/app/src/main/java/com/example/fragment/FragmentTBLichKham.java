0c84550cfa56d584367478ce0cbb62d31ca93485
nhwthao <thaohtn19411@st.uel.edu.vn>
2021-12-25 18:21:40 +0700

    Them xem TBLichKham va xem bao TBYTe

--- Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentTBLichKham.java
+++ Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentTBLichKham.java
@@ -5,19 +5,37 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.wasabiapp.R;
 
 public class FragmentTBLichKham extends Fragment {
+    LinearLayout lnMaLichKham0001;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tblich_kham, container, false);
+        View view = inflater.inflate(R.layout.fragment_tblich_kham, container, false);
+        lnMaLichKham0001 = view.findViewById(R.id.lnMaLichKham0001);
+        addEvents();
+        return view;
+    }
+
+    private void addEvents() {
+        lnMaLichKham0001.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentBookAppointmentNewDetails details = new FragmentBookAppointmentNewDetails();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frTBLichKham, details);
+                transaction.commit();
+            }
+        });
     }
 }
--- Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentTBYTe.java
+++ Back up/WasabiApp/app/src/main/java/com/example/fragment/FragmentTBYTe.java
@@ -2,21 +2,40 @@ package com.example.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.wasabiapp.R;
 
 public class FragmentTBYTe extends Fragment {
+    LinearLayout lnYTeNews;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tby_te, container, false);
+        View view = inflater.inflate(R.layout.fragment_tby_te, container, false);
+        lnYTeNews = view.findViewById(R.id.lnYTeNews);
+        addEvents();
+        return view;
+    }
+
+    private void addEvents() {
+        lnYTeNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentNewsDetails newsDetails = new FragmentNewsDetails();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frTBYTe, newsDetails);
+                transaction.commit();
+            }
+        });
     }
 }
